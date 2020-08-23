3b5f6a23d846afc8c5f77efbfe48dbca0728cd92
Joe A. Spandrusyszyn <jspandrusyszyn@rocsoft.com>
2020-08-23 13:46:24 -0400

    Initial release

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# FoundryVTT-chat-enhancements
+# Illandril's Chat Enhancements
+This is a module for Foundry Virtual Tabletop that adds a few enhancements to the chat.
+
+![Screenshot showing the chat of "Joe (GM)" speaking as Jacreth Stewart](/screenshots/example-a.png?raw=true)
+
+It currently does the following:
+1. Shows the name of the player next to the name of the actor for any IC messages
+1. Shows the name of the actor you are currently controlling
+1. Replaces Actor names with Token names
+  * In the event the token associated with a chat message no longer exists, it will use the actor's prototype token's name. In the event the actor no longer exists, it will show "???" to all players, and the original chat message's alias (usually the actor's name) to the GM.
+1. Hovering over a chat message act as if you hovered over the speaker's token 
+
+# Installation
+1. Open the Configuration and Setup for your FoundryVTT server
+1. Open the Add-on Modules Tab
+1. Click the Install Module button
+1. In the Manifest URL input, specify https://github.com/illandril/FoundryVTT-chat-enhancements/releases/latest/download/module.json
+1. Click Install
+1. Launch your world
+1. Log in as the GM
+1. Open the Settings tab
+1. Click Manage Modules
+1. Check the checkbox for Illandril's Chat Enhancements
---
+++ init.js
@@ -0,0 +1,127 @@
+const CSS_PREFIX = 'illandril-chat-enhancements--';
+const CSS_CURRENT_SPEAKER = CSS_PREFIX + 'currentSpeaker';
+const CSS_PLAYER_NAME = CSS_PREFIX + 'playerName';
+
+const currentSpeakerDisplay = document.createElement('div');
+currentSpeakerDisplay.classList.add(CSS_CURRENT_SPEAKER);
+
+function updateSpeaker() {
+  currentSpeakerDisplay.innerText = game.i18n.format('illandril-chat-enhancements.currentSpeaker', {
+    name: ChatMessage.getSpeaker().alias,
+  });
+}
+
+Hooks.once('ready', () => {
+  const chatControls = document.getElementById('chat-controls');
+  chatControls.parentNode.insertBefore(currentSpeakerDisplay, chatControls);
+  updateSpeaker();
+});
+
+Hooks.on('controlToken', updateSpeaker);
+
+function isWhisperToOther(speakerInfo) {
+  const whisper = speakerInfo.message.whisper;
+  return whisper && whisper.length > 0 && whisper.indexOf(game.userId) === -1;
+}
+
+Hooks.on('renderChatMessage', (message, html, speakerInfo) => {
+  if (isWhisperToOther(speakerInfo)) {
+    // Don't update whispers that the current player isn't privy to
+    return;
+  }
+  const speaker = speakerInfo.message.speaker;
+  if (speaker && speaker.actor) {
+    // It's a chat message associated with an actor
+
+    const messageSenderElem = html.find('.message-sender');
+    replaceSenderWithTokenName(messageSenderElem, speaker);
+    appendPlayerName(messageSenderElem, speakerInfo.author);
+
+    messageSenderElem.hover((event) => {
+      hoverIn(event, speaker);
+    }, hoverOut);
+  }
+});
+
+function replaceSenderWithTokenName(messageSenderElem, speaker) {
+  messageSenderElem.text(getTokenName(speaker));
+}
+
+function appendPlayerName(messageSenderElem, author) {
+  const playerName = author.name;
+  const playerNameElem = document.createElement('span');
+  playerNameElem.appendChild(document.createTextNode(playerName));
+  playerNameElem.classList.add(CSS_PLAYER_NAME);
+  messageSenderElem.append(playerNameElem);
+}
+
+function getTokenName(speaker) {
+  if (speaker.token) {
+    const token = getToken(speaker.scene, speaker.token);
+    if (token) {
+      return token.name;
+    }
+  }
+  const actor = game.actors.get(speaker.actor);
+  if (actor) {
+    if (actor.data.token) {
+      return actor.data.token.name;
+    }
+    if (actor.isPC) {
+      return actor.name;
+    }
+  }
+  if (game.user.isGM) {
+    return speaker.alias;
+  }
+  return '???';
+}
+
+function getToken(sceneID, tokenID) {
+  const scene = game.scenes.get(sceneID);
+  if (!scene) {
+    return null;
+  }
+  return scene.data.tokens.find((token) => {
+    return token._id === tokenID;
+  });
+}
+
+function emptyNode(node) {
+  while (node.firstChild) {
+    node.removeChild(node.lastChild);
+  }
+}
+
+function getTokenObj(id) {
+  return canvas.tokens.get(id);
+}
+
+let lastHoveredToken = null;
+function hoverIn(event, speaker) {
+  let token = getTokenObj(speaker.token);
+  if (!token) {
+    const scene = game.scenes.get(game.user.viewedScene);
+    if (scene) {
+      const thisSceneToken = scene.data.tokens.find((token) => {
+        return token.actorLink && token.actorId === speaker.actor;
+      });
+      if (thisSceneToken) {
+        token = getTokenObj(thisSceneToken._id);
+      }
+    }
+  }
+  if (token && token.isVisible) {
+    event.fromChat = true;
+    token._onHoverIn(event);
+    lastHoveredToken = token;
+  }
+}
+
+function hoverOut(event) {
+  if (lastHoveredToken && lastHoveredToken._hover) {
+    event.fromChat = true;
+    lastHoveredToken._onHoverOut(event);
+    lastHoveredToken = null;
+  }
+}
